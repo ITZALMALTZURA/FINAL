f63ab690eb7025c93b8ba7d74137d98d61ea3d73
ITZALMALTZURA <58190969+ITZALMALTZURA@users.noreply.github.com>
2020-06-13 13:23:38 -0500

    resistencia de un alambre
    requisitos: visual studio con c#.
    instrucciones: para descargar el poryecto solo se debe copiar el link y pegarlo en GITZIP, una vez descomprimido el proyecto se debe abrir DESDE visual studio.

---
+++ RESISTIVIDAD/RESISTIVIDAD/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace RESISTIVIDAD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.Color.White;
+            this.trackBar1.Location = new System.Drawing.Point(152, 97);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Value = 50;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(152, 160);
+            this.trackBar2.Maximum = 200;
+            this.trackBar2.Minimum = 1;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 1;
+            this.trackBar2.Value = 100;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(102, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Resistencia =.667Ω ";
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(152, 223);
+            this.trackBar3.Maximum = 1500;
+            this.trackBar3.Minimum = 1;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 45);
+            this.trackBar3.TabIndex = 3;
+            this.trackBar3.Value = 750;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(281, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = ".50Ω cm";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(281, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "10cm";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(281, 233);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "7.5cm²";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Blue;
+            this.label5.Location = new System.Drawing.Point(34, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "P Resistividad";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Blue;
+            this.label6.Location = new System.Drawing.Point(34, 169);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 16);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "L Longitud";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Blue;
+            this.label7.Location = new System.Drawing.Point(34, 233);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 16);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "A Area";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.trackBar2);
+            this.groupBox1.Controls.Add(this.trackBar3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(401, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(387, 415);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::RESISTIVIDAD.Properties.Resources.reset;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Location = new System.Drawing.Point(183, 322);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 60);
+            this.button1.TabIndex = 10;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::RESISTIVIDAD.Properties.Resources.alambre;
+            this.pictureBox1.Location = new System.Drawing.Point(161, 265);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 41);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::RESISTIVIDAD.Properties.Resources.resistencia;
+            this.pictureBox2.Location = new System.Drawing.Point(23, 86);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 52);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::RESISTIVIDAD.Properties.Resources.resistividad;
+            this.pictureBox3.Location = new System.Drawing.Point(145, 32);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(50, 67);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 13;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::RESISTIVIDAD.Properties.Resources.longitud;
+            this.pictureBox4.Location = new System.Drawing.Point(231, 38);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(50, 61);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 14;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::RESISTIVIDAD.Properties.Resources.area;
+            this.pictureBox5.Location = new System.Drawing.Point(175, 147);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox5.TabIndex = 15;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::RESISTIVIDAD.Properties.Resources.flecha;
+            this.pictureBox6.Location = new System.Drawing.Point(161, 343);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(100, 25);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox6.TabIndex = 16;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::RESISTIVIDAD.Properties.Resources.igual;
+            this.pictureBox7.Location = new System.Drawing.Point(90, 105);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(200, 21);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox7.TabIndex = 17;
+            this.pictureBox7.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox7);
+            this.Name = "Form1";
+            this.Text = "Resistencia en un alambre";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+    }
+}
+
---
+++ RESISTIVIDAD/RESISTIVIDAD/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace RESISTIVIDAD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SoundPlayer Sound;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //resistividad
+            trackBar1.Maximum = 100;
+            trackBar1.Minimum = 1;
+            trackBar1.SmallChange = 1;
+            trackBar1.LargeChange = 1;
+            trackBar1.UseWaitCursor = false;
+            //longitud
+            trackBar2.Maximum = 200;
+            trackBar2.Minimum = 1;
+            trackBar2.SmallChange = 1;
+            trackBar2.LargeChange = 1;
+            trackBar2.UseWaitCursor = false;
+            //area
+            trackBar3.Maximum = 1500;
+            trackBar3.Minimum = 1;
+            trackBar3.SmallChange = 1;
+            trackBar3.LargeChange = 1;
+            trackBar3.UseWaitCursor = false;
+            this.DoubleBuffered = true;
+            //instancias 
+            org1 = new PictureBox();
+            org1.Image = pictureBox1.Image;//barra
+            org2 = new PictureBox();
+            org2.Image = pictureBox2.Image;//resistencia
+            org3 = new PictureBox();
+            org3.Image = pictureBox3.Image;//reisistividad
+            org4 = new PictureBox();
+            org4.Image = pictureBox4.Image;//longitud
+            org5 = new PictureBox();
+            org5.Image = pictureBox5.Image;//area
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            int TB1 = trackBar1.Value;//asignamos el valor del trackbar a una variable de tipo entero(1-100),Resistividad
+            double TBR = TB1 * (.01);//convertimos el valor del trackbar a punto decimal 1 =.01 y 100 = 1
+            int TB3 = trackBar2.Value;//asignamos el valor del trackbar a una variable de tipo entero(1-200),Longitud
+            double TBL = TB3 * (.1);//convertimos el valor del trackbar a punto decimal 1 =.1 y 200 = 20
+            int TB4 = trackBar3.Value;//asignamos el valor del trackbar a una variable de tipo entero(1-150),Area
+            double TBA = TB4 * (.01);//convertimos el valor del trackbar a punto decimal 1 =.01 y 1500 = 15
+            double R1 = TBR * TBL;// la formula es P*L/A
+            double R2 = R1 / TBA;
+
+
+            label1.Text = "Resistencia =" + R2.ToString() + "Ω";// asignamos la resistencia del alambre al label(mostramos el resultado final)
+            label2.Text = TBR.ToString() + "Ω cm";//asignamos la resistividad del trackbar al label
+            label3.Text = TBL.ToString() + "cm";//asignamos la longitud del alambre al label
+            label4.Text = TBA.ToString() + "cm²";//asignamos el area de alambre al label
+
+            if (trackBar1.Value > 50)
+            {
+                pictureBox3.Image = Zoom3(org3.Image, new Size(trackBar1.Value, trackBar1.Value));//resisitividad aumenta
+                pictureBox2.Image = Zoom3(org2.Image, new Size(trackBar1.Value, trackBar1.Value));//resistencia aumenta
+            }
+            if (trackBar1.Value < 50)
+            {
+                pictureBox3.Image = Zoom3(org3.Image, new Size(trackBar1.Value, trackBar1.Value));//resisitividad disminulle
+                pictureBox2.Image = Zoom3(org2.Image, new Size(trackBar1.Value, trackBar1.Value));//resistencia disminulle
+            }
+
+
+        }
+        Image Zoom1(Image img,Size size)
+        {
+            //anchura
+            Bitmap bmp1 = new Bitmap(img, Convert.ToInt32(img.Width * size.Width/100), Convert.ToInt32(img.Height));
+            Graphics g = Graphics.FromImage(bmp1);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            return bmp1;
+
+        }
+        Image Zoom2(Image img, Size size)
+        {
+            //altura
+            Bitmap bmp2 = new Bitmap(img, Convert.ToInt32(img.Width), Convert.ToInt32(img.Height * size.Height/ 40));
+            Graphics g = Graphics.FromImage(bmp2);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            return bmp2;
+
+        }
+        Image Zoom3(Image img, Size size)
+        {
+            //resistencia, longitud,resistividad
+            Bitmap bmp3 = new Bitmap(img, Convert.ToInt32(img.Width + size.Width / 5), Convert.ToInt32(img.Height + size.Height / 4));
+            Graphics g = Graphics.FromImage(bmp3);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            return bmp3;
+        }
+
+        Image Zoom4(Image img, Size size)
+        {
+            //area
+            Bitmap bmp4 = new Bitmap(img, Convert.ToInt32(img.Width + size.Width / 40), Convert.ToInt32(img.Height + size.Height / 40));
+            Graphics g = Graphics.FromImage(bmp4);
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+            return bmp4;
+        }
+        PictureBox org1;//barra
+        PictureBox org2;//resistencia
+        PictureBox org3;//resistividad
+        PictureBox org4;//longitud
+        PictureBox org5;//area
+        
+        private void trackBar2_Scroll(object sender, EventArgs e)//longitud
+        {
+            int TB1 = trackBar1.Value;
+            double TBR = TB1 * (.01);
+            int TB3 = trackBar2.Value;
+            double TBL = TB3 * (.1);
+            int TB4 = trackBar3.Value;
+            double TBA = TB4 * (.01);
+            double R1 = TBR * TBL;
+            double R2 = R1 / TBA;
+
+
+            label1.Text = "Resistencia =" + R2.ToString() + "Ω";
+            label2.Text = TBR.ToString() + "Ω cm";
+            label3.Text = TBL.ToString() + "cm";
+            label4.Text = TBA.ToString() + "cm²";
+            //ancho de la imagen
+
+            if (trackBar2.Value > 75)
+            {
+                pictureBox4.Image = Zoom3(org4.Image, new Size(trackBar2.Value, trackBar2.Value));//longitud aumenta
+                pictureBox1.Image = Zoom1(org1.Image, new Size(trackBar2.Value, trackBar2.Value));// barra aumenta
+            }
+            if (trackBar2.Value < 75)
+            {
+                pictureBox4.Image = Zoom3(org4.Image, new Size(trackBar2.Value, trackBar2.Value));//longitud disminulle
+                pictureBox1.Image = Zoom1(org1.Image, new Size(trackBar2.Value, trackBar2.Value));//barra disminulle
+            }
+           
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            int TB1 = trackBar1.Value;
+            double TBR = TB1 * (.01);
+            int TB3 = trackBar2.Value;
+            double TBL = TB3 * (.1);
+            int TB4 = trackBar3.Value;
+            double TBA = TB4 * (.01);
+            double R1 = TBR * TBL;
+            double R2 = R1 / TBA;
+
+
+            label1.Text = "Resistencia =" + R2.ToString() + "Ω";
+            label2.Text = TBR.ToString() + "Ω cm";
+            label3.Text = TBL.ToString() + "cm";
+            label4.Text = TBA.ToString() + "cm²";
+
+            if (trackBar3.Value > 750)
+            {
+                pictureBox5.Image = Zoom4(org5.Image, new Size(trackBar3.Value, trackBar3.Value));//area aumenta
+            }
+            if (trackBar3.Value < 750)
+            {
+                pictureBox5.Image = Zoom4(org5.Image, new Size(trackBar3.Value, trackBar3.Value));//area disminulle
+            }
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Sound =  new SoundPlayer(Application.StartupPath + @"\songs\recharge.wav");
+                Sound.Play();
+                
+                Application.Restart();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex);
+            }
+            
+            
+        }
+
+        
+    }
+}
